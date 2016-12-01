cc3a76e9236e5d747a447d3654385d48a19de51e
Ludovic <ludovictichit3@gmail.com>
2016-12-01 15:58:13 +0100

    [LTI]Main structure

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Information retrieval : Search engine
 by
 Julien Da Costa Vaz, Dimitri Escalle, Pierre Gourseaud, Ludovic Tichit
+
+# Team name
+PierreJulienDimitriLudovic
---
+++ main.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# postingList = parsing()
+
+# ltnResults = ltn(postingList)
+# ltcResults = ltc(postingList)
+
+# writeRuns(ltnResults)
+# writeRuns(ltcResults)

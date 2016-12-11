7ff6c1bf5f1b0c49cdab3c4052addd520ef265be
Ludovic <ludovictichit3@gmail.com>
2016-12-11 19:06:53 +0100

    [LTI]Ajout du bm25, manque avgdl et le taille de chaque doc

--- main.pl
+++ main.pl
@@ -15,10 +15,11 @@ my @requests = (
     'operating system mutual exclusion');
 
 
-my @writingList = ();
+my @writingListLtn = ();
+my @writingListBm25 = ();
 my $compteur = 0;
 
-my $nbDocuments = 9800;
+my $nbDocuments = 9804;
 my %postingList = parser::parsing();
 # my %postingList = (
 #     'olive' => {'doc1' => '5', 'doc2' => '1'},
@@ -27,36 +28,68 @@ my %postingList = parser::parsing();
 
 for my $request (@requests) {
     my @requestTerms = split(/ /, $request);
-    my %RSV = ();
+    my %RSVltn = ();
+    my %RSVbm25 = ();
     for my $term (@requestTerms) {
         if(exists $postingList{$term}) {
             my %hash = %{$postingList{$term}};
             for my $key (keys %hash) {
-                my $score = ltn($hash{$key}, scalar(keys %hash));
-                if(exists $RSV{$key}) {
-                    $RSV{$key} = $RSV{$key} + $score;
+                my $scoreLtn = ltn($hash{$key}, scalar(keys %hash));
+                my $scoreBm25 = bm25($hash{$key}, scalar(keys %hash), 1, 0.5);
+                if(exists $RSVltn{$key}) {
+                    $RSVltn{$key} = $RSVltn{$key} + $scoreLtn;
                 } else {
-                    $RSV{$key} = $score;
+                    $RSVltn{$key} = $scoreLtn;
+                }
+                if(exists $RSVbm25{$key}) {
+                    $RSVbm25{$key} = $RSVbm25{$key} + $scoreBm25;
+                } else {
+                    $RSVbm25{$key} = $scoreBm25;
                 }
             }
         }
     }
     $compteur = 0;
-    foreach my $doc ( reverse (sort { $RSV{$a} <=> $RSV{$b} } keys %RSV) ) {
-        printf "%-8s %s\n", $doc, $RSV{$doc};
-        push(@writingList, $doc);
+    foreach my $doc ( reverse (sort { $RSVltn{$a} <=> $RSVltn{$b} } keys %RSVltn) ) {
+        printf "%-8s %s\n", $doc, $RSVltn{$doc};
+        push(@writingListLtn, $doc);
         $compteur++;
         last if ($compteur == 1500);
     }
-#
-#     for my $k (keys %RSV){
-#   print "$k : $RSV{$k}\n";
-# }
+
+    $compteur = 0;
+    foreach my $doc ( reverse (sort { $RSVbm25{$a} <=> $RSVbm25{$b} } keys %RSVbm25) ) {
+        printf "%-8s %s\n", $doc, $RSVbm25{$doc};
+        push(@writingListBm25, $doc);
+        $compteur++;
+        last if ($compteur == 1500);
+    }
 
     # Sort RSV by increasing score and keep the best 1500 results
     # Write the 1500 result lines for the current request
-    writer::writeRuns("02", "ltn", @writingList);
+    writer::writeRuns("02", "ltn", @writingListLtn);
+    writer::writeRuns("02", "bm25", @writingListBm25);
 }
+
+sub bm25 {
+    my $tf = shift;
+    my $df = shift;
+    my $k = shift;
+    my $b = shift;
+    my $newTf = ($tf*($k+1))/($k*((1 + $b) + $b * (docLengths[docNo] / avgdl)) + $tf);
+    my $newIdf = log(($nbDocuments - $df + 0.5)/($df + 0.5));
+    return $newTf*$newIdf;
+}
+
+# bm25(term, docNo, k, b){ //On peut prendre k=1 et b=0.5 ; 0<b<1 ; 0<k<+infini
+# 	hash = postList[term]
+# 	tf = hash[docNo]
+# 	df = length(hash) //nb de documents dans lesquels apparait le terme
+# 	newtf = (tf*(k+1))/(k*((1+b)+b*(docLengths[docNo]/avgdl))+tf)
+# 	newidf = log((nbDoc-df+0.5)/(df+0.5))
+# 	bm25 = newtf*newidf
+# 	return bm25
+# }
 
 sub ltn {
     my $tf = shift;
